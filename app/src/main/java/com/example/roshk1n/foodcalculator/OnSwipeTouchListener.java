171a0c98c2adac64051fbf2e3f0348e83eeb8808
roshk1n <oleh.roshka@outlook.com>
2016-09-26 01:27:22 +0300

    hotfix/ delete space

--- app/src/main/java/com/example/roshk1n/foodcalculator/OnSwipeTouchListener.java
+++ app/src/main/java/com/example/roshk1n/foodcalculator/OnSwipeTouchListener.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.View.OnTouchListener;
 
 public class OnSwipeTouchListener implements OnTouchListener {
-
     private final GestureDetector gestureDetector;
 
     public OnSwipeTouchListener (Context ctx){
